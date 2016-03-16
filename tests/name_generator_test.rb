ecf9f957017a3cbba5bcb6d30b62fdf803da8e9f
Shawn Michaud <shawnmne@hotmail.com>
2016-03-16 15:17:27 -0500

    some test cases identified

--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -3,3 +3,9 @@ require_relative '../lib/name_generator.rb'
 # Define whatever methods you need first
 
 # Now run your actual tests, making sure to do output as necessary
+
+#Shawn Michaud => xX_ShAwNmIcHaUd_Xx pass
+#Sh5wn Michaud => xX_ShWnMiChAuD_Xx pass
+#Shawn Michaud => xX_ShAwN mIcHaUd_XX fail
+#Shawn Mi&&&& =>  xX_ShAwNmI&&&&&_Xx fail
+#""  =>  xX__Xx fail do to empty string
