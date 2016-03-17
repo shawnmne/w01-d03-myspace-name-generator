82442e58b591f3528146a008d11899e5f776adf4
Shawn Michaud <shawnmne@hotmail.com>
2016-03-16 23:19:43 -0500

    working on test cases

--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -1,4 +1,6 @@
 require_relative '../lib/name_generator.rb'
+require "test/unit/assertions"
+include Test::Unit::Assertions
 
 # Define whatever methods you need first
 
@@ -7,4 +9,20 @@ require_relative '../lib/name_generator.rb'
 
 #first test
 result = lowercase_name("ShawnMichaUD")
-assert_equal("shawnmichaud", result, ":)")
+assert_equal("shawnmichaud", result, "failure")
+
+#second test
+result = remove_punctuation(" shawn")
+assert_equal("shawn", result, "failure")
+
+#third test
+result = remove_punctuation("sh&awn")
+assert_equal("shawn", result, "failure")
+
+#fourth test 
+result = remove_punctuation("sh\nawn")
+assert_equal("shawn", result, "failure")
+
+#fifth test 
+result = remove_punctuation("shawn ")
+assert_equal("shawn", result, "failure")
