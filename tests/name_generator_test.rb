0b822a5870b76aeea6e96365c42c162b77590148
Shawn Michaud <shawnmne@hotmail.com>
2016-03-16 23:27:59 -0500

    all tests built

--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -26,3 +26,23 @@ assert_equal("shawn", result, "failure")
 #fifth test 
 result = remove_punctuation("shawn ")
 assert_equal("shawn", result, "failure")
+
+#6 test
+result = remove_numbers("sha99wn")
+assert_equal("shawn", result, "failure")
+
+#7 test
+result = remove_numbers("8shawn123")
+assert_equal("shawn", result, "failure")
+
+#8 test
+result = capitalize_every_other("shawn")
+assert_equal("ShAwN", result, "failure")
+
+#9 test
+result = capitalize_every_other("landon")
+assert_equal("LaNdOn", result, "failure")
+
+#10 test
+result = add_characters("shawn")
+assert_equal("xX_shawn_Xx", result, "failure")
