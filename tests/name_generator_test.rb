bba22e23a76b512e49ff1a783c5aa243da37a14e
Shawn Michaud <shawnmne@hotmail.com>
2016-03-16 23:01:20 -0500

    first test written

--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -4,8 +4,7 @@ require_relative '../lib/name_generator.rb'
 
 # Now run your actual tests, making sure to do output as necessary
 
-#Shawn Michaud => xX_ShAwNmIcHaUd_Xx pass
-#Sh5wn Michaud => xX_ShWnMiChAuD_Xx pass
-#Shawn Michaud => xX_ShAwN mIcHaUd_XX fail
-#Shawn Mi&&&& =>  xX_ShAwNmI&&&&&_Xx fail
-#""  =>  xX__Xx fail do to empty string
+
+#first test
+result = lowercase_name("ShawnMichaUD")
+assert_equal("shawnmichaud", result, ":)")
