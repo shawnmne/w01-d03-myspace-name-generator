750564d75e903f8e2a887264efdfbdc05bd0566b
Shawn Michaud <shawnmne@hotmail.com>
2016-03-17 13:13:08 -0500

    added minitest

--- tests/name_generator_test.rb
+++ tests/name_generator_test.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/name_generator.rb'
 require "test/unit/assertions"
 include Test::Unit::Assertions
-
-result = false
+def test_name_generator
+result = true
 assert(result,"this should have not failed")
+end
